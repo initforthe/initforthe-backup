b6b4637bb261a5a3cd0ba7519b3f05a2aa2c0b2e
Tom Simnett <tom@initforthe.com>
2017-03-17 10:19:56 +0000

    Add backup config bits

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,9 @@
 # Private class backup::config
-class backup::config {
+class backup::config (
+  $root_path = $::backup::root_path,
+  $tmp_path = $::backup::tmp_path,
+  $data_path = $::backup::data_path,
+){
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
